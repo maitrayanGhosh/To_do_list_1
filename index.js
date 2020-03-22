9bf2c0c90c3ca520a4eab232186f43722c485bf4
unknown <maitrayan6p@gmail.com>
2020-03-22 12:14:41 +0530

    second commit

--- index.js
+++ index.js
@@ -15,10 +15,53 @@ app.use(express.urlencoded())
 app.use(express.static('assets'))
 
 
+var taskList=[
+    {
+        description : "milk",
+        catagory : "personal",
+        dueDate : "11/11/2020"
+    },
+
+    {
+        description : "chicken",
+        catagory : "personal",
+        dueDate : "13/11/2020"
+    },
+
+    {
+        description : "hw",
+        catagory : "school",
+        dueDate : "12/11/2020"
+    },
+]
+
+
 
 // controller
 app.get('/',(req,res)=>{
-    return res.render('home')
+    return res.render('home',{
+        title: "To do list",
+        test : "cat",
+        task_list : taskList
+    })
+})
+
+
+
+app.post('/create_task',(req,res)=>{
+  
+    // Contact.create({
+    //     name : req.body.name,
+    //     phone : req.body.phone
+    // },(err,newContact)=>{
+    //     if(err){
+    //         console.log('error')
+    //         return
+    //     }
+    //     console.log('******', newContact)
+    //     return res.redirect('back')
+    // })
+    
 })
 
 
